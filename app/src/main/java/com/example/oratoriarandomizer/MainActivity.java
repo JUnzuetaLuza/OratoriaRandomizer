1cc216bb80eef7b07f51595e3f8da044f2f908d0
JUnzuetaLuza <joseunz2k@gmail.com>
2024-05-04 23:09:08 -0500

    Genera Num Random

--- app/src/main/java/com/example/oratoriarandomizer/MainActivity.java
+++ app/src/main/java/com/example/oratoriarandomizer/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.oratoriarandomizer;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public void randomNum(View view) {
+
+        // Declarar variables
+        Button btnGenerate = findViewById(R.id.btnGenerate);
+        TextView txtRandomNum = findViewById(R.id.txtRandomNum);
+
+        // Generar un numero aleatorio del 1 al 28
+        int randomNumber = generateRandomNum(1, 28);
+        // Mostrar el numero aleatorio en el TextView
+        txtRandomNum.setText(String.valueOf(randomNumber));
+    }
+
+    private int generateRandomNum(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
     }
 }
